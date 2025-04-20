486afd5b18cc1804aadbf2357dc57caeedeb745a
warp5t <dmitrij.didencko@yandex.by>
2025-04-20 22:55:11 +0300

    feat: implement StartScreen compoment

---
+++ src/components/startScreen/StartScreen.tsx
@@ -0,0 +1,29 @@
+
+export const StartScreen = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <input type="text" /><input type="text" />
+        </div>
+        <div>
+          <select name="category" id=""></select>
+        </div>
+        <div>
+          <select name="difficulty" id=""></select>
+        </div>
+        <div>
+          <select name="type" id=""></select>
+        </div>
+        <div>
+          <select name="time" id=""></select>
+        </div>
+      </div>
+      <div>
+        <button>Start</button>
+        <button>See my stats</button>
+      </div>
+    </div>
+  )
+}
+
