173e2568ef4f76d49e3616fc036bee461ab87cd6
warp5t <dmitrij.didencko@yandex.by>
2025-05-12 11:19:46 +0300

    feat: implement quiz config screen

--- src/screen/configScreen/ConfigScreen.tsx
+++ src/screen/configScreen/ConfigScreen.tsx
@@ -16,12 +16,51 @@ export const ConfigScreen = () => {
 
   return (
     <div>
-      <h2>Category</h2>
+      <h3>Number of questions</h3>
+      <select name="" id="">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+      </select>
+      <h3>Category</h3>
       <select name="" id="">
         {categoryQuest.map((el) => (
-          <option key={el.id}>{el.name}</option>
+          <option key={el.id} value={el.name}>{el.name}</option>
         ))}
       </select>
+      <h3>Difficulty</h3>
+      <select name="" id="">
+        <option value="any">Any Difficulty</option>
+        <option value="easy">Easy</option>
+        <option value="medium">Medium</option>
+        <option value="hard">Hard</option>
+      </select>
+      <h3>Type</h3>
+      <select name="" id="">
+        <option value="any">Any Type</option>
+        <option value="multiple">Multiple Choise</option>
+        <option value="boolean">True/False</option>
+      </select>
+      <h3>Time</h3>
+        <select name="" id="">
+          <option value="1">1 Minute</option>
+          <option value="2">2 Minutes</option>
+          <option value="5">5 Minutes</option>
+        </select>
+        <button>Start Quiz</button>
+        <button>See my stats</button>
       <button onClick={() => { console.log(categoryQuest) }}>Log Categories</button>
     </div>
   );
