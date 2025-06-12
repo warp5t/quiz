ad85dab001d33d7f7018bda5132db93b0898afc9
warp5t <dmitrij.didencko@yandex.by>
2025-06-12 13:55:53 +0300

    feat: implement iterating questions

--- src/screen/mainScreen/MainScreen.tsx
+++ src/screen/mainScreen/MainScreen.tsx
@@ -3,23 +3,19 @@ import { useDispatch } from "react-redux";
 import { RootState } from "../../store/store"
 import { AppDispatch } from "../../store/store";
 import { sendAnswer } from "../../slice/progressSlice/progressSlice";
-import { useMemo, useState } from "react";
-// import { motion } from 'framer-motion';
-
+import { useState } from "react";
 
 import { WinAnimation } from "../../components/animation/winAnimation/WinAnimation";
 import { Confetti } from "../../components/animation/confetti/Confetti";
 
 export default () => {
   const [isVisible, setIsVisibleC] = useState(false);
+  const [index, setIndex] = useState(0);
   const stateConfigQuiz = useSelector((state: RootState) => state.configSliceStart.results);
   const stateProgress = useSelector((state: RootState) => state.configSliceProgress);
   const dispatch = useDispatch<AppDispatch>();
-  // const [showConfetti, setShowConfetti] = useState(false);
+  const ammountQuestion = stateConfigQuiz.length;
 
-  // const ammountQuestion = stateConfigQuiz.length;
-
-  let index = 0;
   const type = stateConfigQuiz[index].type;
 
   const decodeEntities = (text: string) => {
@@ -30,7 +26,6 @@ export default () => {
 
   const winChange = () => {
     setIsVisibleC((isVisible) => !isVisible)
-    // setShowConfetti(true)
   }
 
   const handleAnswer = (answer: string) => {
@@ -49,13 +44,14 @@ export default () => {
     if (correctAnswer === answer) {
       winChange()
     }
+    if (index < ammountQuestion - 1 && index !== ammountQuestion - 1) {
+      setIndex((index) => index += 1)
+    }
   };
 
   if (type === 'multiple') {
-    const memoQuestArr = useMemo(() => {
-      const arrQuest = [...stateConfigQuiz[index].incorrect_answers, stateConfigQuiz[index].correct_answer];
-      return [...arrQuest].sort(() => Math.random() - 0.5);
-    }, [index]);
+    const arrQuest = [...stateConfigQuiz[index].incorrect_answers, stateConfigQuiz[index].correct_answer];
+    const jumbleQuest = [...arrQuest].sort(() => Math.random() - 0.5);
 
     return (
       <div>
@@ -63,7 +59,7 @@ export default () => {
         <p>Difficulty: {stateConfigQuiz[index].difficulty}</p>
         <p>{decodeEntities(stateConfigQuiz[index].question)}</p>
         <div>
-          {memoQuestArr.map((answer) => (
+          {jumbleQuest.map((answer) => (
             <button key={answer} onClick={() => {
               handleAnswer(answer);
               answerChecking(answer);
@@ -75,6 +71,8 @@ export default () => {
         <button onClick={() => {
           console.log('stateConfigQuiz: ', stateConfigQuiz);
           console.log('stateProgress: ', stateProgress);
+          console.log('index: ', index);
+
         } }>LOG</button>
         <WinAnimation  isVisible={isVisible} setIsVisible={winChange}/>
         {isVisible && <Confetti />}
